becc71b1255224368b741ff9314a58b414e1386f
RobinLoksli92 <gleb11tiun@mail.ru>
2021-09-26 12:31:23 +0300

    2nd_commit

--- keyboards.py
+++ keyboards.py
@@ -10,7 +10,6 @@ keyboard_add = [
     [InlineKeyboardButton('Ввести название', callback_data='add_title')],
     [InlineKeyboardButton('Отправить фото вещи', callback_data='add_photo')],
     [InlineKeyboardButton('Готово', callback_data='done')],
-    [InlineKeyboardButton('Назад', callback_data='back')],
 ]
 
 keyboard_find = [
@@ -18,5 +17,4 @@ keyboard_find = [
         InlineKeyboardButton('Не нравится', callback_data='not'),
         InlineKeyboardButton('Нравится', callback_data='like')
     ],
-    [InlineKeyboardButton('Назад', callback_data='back')],
 ]
